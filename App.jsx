a754c9dd6a9d459afb4adb5150025d48636898bc
karthick_palanivel <karthickpalanivel4@gmail.com>
2024-08-08 16:10:29 +0530

    some changes

--- App.jsx
+++ App.jsx
@@ -1,12 +1,29 @@
+/* eslint-disable prettier/prettier */
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return(
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>App</Text>
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="HOME" component={Home} />
+          <Tab.Screen name="ORDERS" component={Home} />
+          <Tab.Screen name="CART" component={Home} />
+          <Tab.Screen name="ACCOUNT" component={Home} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
