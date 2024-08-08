9b725718c5195032e8da8245116d568d01097ed0
karthick_palanivel <karthickpalanivel4@gmail.com>
2024-08-08 17:53:26 +0530

    added some styles

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
@@ -13,13 +14,18 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: 'red',
+        }}>
           <Tab.Screen name="HOME" component={Home} />
           <Tab.Screen name="ORDERS" component={Orders} />
           <Tab.Screen name="CART" component={Cart} />
           <Tab.Screen name="ACCOUNT" component={Accounts} />
         </Tab.Navigator>
       </NavigationContainer>
+
   );
 };
 
